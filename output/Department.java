7c1bd5f0f122f81bfab67a2b3bb778d59d7445e8
vineelrudrapati <vineelrudrapati9@gmail.com>
2026-06-26 01:32:01 +0530

    first phase

--- output/Department.java
+++ output/Department.java
@@ -1,3 +1,11 @@
-public class Department {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "department")
+public class Department {
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	@Column(name = "name")
+	private String name;
 }
--- output/Employee.java
+++ output/Employee.java
@@ -1,3 +1,14 @@
-public class Employee {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "employee")
+public class Employee {
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	@Column(name = "name")
+	private String name;
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
 }
